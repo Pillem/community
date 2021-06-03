2c4e5e2cf23d76fc5684ec4d9cefec39213dc569
Pim de Vroom <p.j.vroom@students.uu.nl>
2021-06-03 16:54:58 +0200

    Fixed wrong graph usage by other model, added 100 average for single datasettings

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import networkx as nx
 from networkx.algorithms.cluster import average_clustering
 from networkx.generators.community import LFR_benchmark_graph
 import subprocess
-
+from sklearn.metrics.cluster import normalized_mutual_info_score as nmi
 import networkx.algorithms.community as nxcom
 from matplotlib import pyplot as plt
+from numpy.core.shape_base import block
 
 def set_node_community(G, communities):
     '''Add community to node attributes'''
@@ -13,6 +14,14 @@ def set_node_community(G, communities):
         for v in v_c:
             # Add 1 to save 0 for external edges
             G.nodes[v]['community'] = c + 1
+
+def get_nodes_community_list(G):
+    '''Get list orderd by nodes with the community label as value'''
+    comms = []
+    for v in G.nodes:
+        # Add 1 to save 0 for external edges
+        comms.append(G.nodes[v]['community'])
+    return comms
 
 def set_edge_community(G):
     '''Find internal edges and add their community to their attributes'''
@@ -36,7 +45,7 @@ def get_color(i, r_off=1, g_off=1, b_off=1):
     return (r, g, b)            
 
 def nodecolours(communities):
-    print("Assigning colours to communities")    
+    # print("Assigning colours to communities")    
     node_to_community = dict()
     index = 0
     for com in communities:
@@ -58,24 +67,23 @@ def nodecolours(communities):
     }
     #node_color = {node: community_to_color[community_id] for node, community_id in node_to_community.items()}
     node_color_array = [community_to_color[community_id] for node, community_id in node_to_community.items()]
-    print("Assigning colours complete")
+    # print("Assigning colours complete")
     return node_color_array
 
-def draw(g, node_colours):
-    print("Build image of graph")  
-    pos =  nx.spectral_layout(g)
-    #pos =  nx.spring_layout(g)
-    # partition = community_louvain.best_partition(g)
-    # print(partition)
+def draw(g, comms, layout):
+    node_colours = nodecolours(comms)
+    # print("Build image of graph")  
+    if(layout == 'spec'):
+        pos =  nx.spectral_layout(g)
+    else:
+        pos =  nx.spring_layout(g)
+    
     plt.rcParams.update(plt.rcParamsDefault)
     plt.rcParams.update({'figure.figsize': (15, 10)})
     plt.style.use('dark_background')
 
-    # communities = sorted(nxcom.greedy_modularity_communities(g), key=len, reverse=True)
-    # print("Found " + str(len(communities))+ " communities")
-
     # Set node and edge communities
-    set_node_community(g, top_level_communities)
+    set_node_community(g, comms)
     set_edge_community(g)
 
     # Set community color for internal edges
@@ -84,101 +92,151 @@ def draw(g, node_colours):
     internal_color = ["black" for e in internal]
     node_colours = [get_color(g.nodes[v]['community']) for v in g.nodes]
     
-    # external edges
-    nx.draw_networkx(
-        g, 
-        pos=pos, 
-        node_size=0, 
-        edgelist=external, 
-        edge_color="silver",
-        node_color=node_colours,
-        alpha=0.1, 
-        with_labels=False)
-    # internal edges
-    nx.draw_networkx(
-        g, 
-        pos=pos, 
-        edgelist=internal, 
-        edge_color=internal_color,
-        node_color=node_colours,
-        alpha=0.05, 
-        with_labels=False)
+    # # external edges
+    # nx.draw_networkx(
+    #     g, 
+    #     pos=pos, 
+    #     node_size=0, 
+    #     edgelist=external, 
+    #     edge_color="silver",
+    #     node_color=node_colours,
+    #     alpha=0.15, 
+    #     with_labels=False)
+    # # internal edges
+    # nx.draw_networkx(
+    #     g, 
+    #     pos=pos, 
+    #     edgelist=internal, 
+    #     edge_color=internal_color,
+    #     node_color=node_colours,
+    #     alpha=0.09, 
+    #     with_labels=False)
 
+    # print("Build image of graph complete")  
 
-    print("Build image of graph complete")  
+    # plt.show(block=False)
 
-    plt.show()
+def readCommunitiesFromFile(fileName):
+    sets = []
+    with open(fileName) as f:
+        content = f.read().splitlines()
+        for line in content:
+            node, comm = line.split()
+            if(int(comm) >= len(sets)):
+                a = {node}
+                sets.append(a)
+            else:
+                sets[int(comm)].add(node)
+    return sets
 
+def getLFR(args):
+    # print("Start LFR generation")
+    arguments = " -N " + str(args['N']) + " -k "     + str(args['k']) + " -maxk "  + str(args['maxk']) + " -mu "    + str(args['mu']) + " -t1 "    + str(args['t1']) + " -t2 "    + str(args['t2']) + " -minc "  + str(args['minc']) + " -maxc "  + str(args['maxc'])
+    try:
+        subprocess.run(["./benchmark" + arguments] , shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except Exception:
+        pass
+    g = nx.read_edgelist('./network.dat')
+    # print("Finnished LFR generation")
+    return g
 
+def getCommunitiesLFR(g):
+    return sorted(readCommunitiesFromFile('./community.dat'), key=len, reverse=True)
 
-# # create a modular graph    WERKT MAAR TE ZWAAR VOOR GN
-N =     1000    # variable 1000 or 5000
-k =     20      # static
-maxk =  50      # static
-mu =    0.6      # For 0 to 1 in steps
-t1 =  2         # static
-t2 =  1         # static
+def getCommunitiesGN(g):
+    # print("Start Girvan Newman for communities")
+    communities_generator = nxcom.girvan_newman(g)
+    comms = sorted(next(communities_generator), key=len, reverse=True)
+    # print("Finnished Girvan Newman")
+    return comms
 
-minc =  10      # variable [10 and 50] or [20 and 100]
-maxc =  50
+def getCommunitiesCLAUSET(g):
+    # print("Start Clauset greedy modularity optimisation for communities")
+    comms = sorted(nxcom.greedy_modularity_communities(g), key=len, reverse=True)
+    # print("Finnished Clauset greedy modularity optimisation for communities")
+    return comms
 
-# input_file = "./input"
+def runSinglesMuIncrease(lfrArgs):
+    for i in range(10):
+        g = getLFR(lfrArgs)
+        g_GN = g.copy()
+        g_CLAUSET = g.copy()
+        
+        top_level_communities = getCommunitiesLFR(g)
+        top_level_communities_GN = getCommunitiesGN(g_GN)
+        top_level_communities_CLAUSET = getCommunitiesCLAUSET(g_CLAUSET)
+        
+        # print("Generated " + str(len(top_level_communities)) + " communities in the graph")
+        # print("GN Found " + str(len(top_level_communities_GN)) + " communities in the graph")
+        # print("CLAUSET Found " + str(len(top_level_communities_CLAUSET)) + " communities in the graph")
+        
+        draw(g, top_level_communities, 'spec')
+        plt.figure(2)
+        draw(g_GN, top_level_communities_GN, 'spec')
+        plt.figure(3)
+        draw(g_CLAUSET, top_level_communities_CLAUSET, 'spec')
+        print("----------------------------------------------------------------------------")
+        print("mu = " + str(lfrArgs['mu']))
+        print("Normalized mutual information GN: " + str(nmi(get_nodes_community_list(g), get_nodes_community_list(g_GN))))
+        print("Normalized mutual information CLAUSET: " + str(nmi(get_nodes_community_list(g), get_nodes_community_list(g_CLAUSET))))
+        print("----------------------------------------------------------------------------")
+        lfrArgs['mu'] += 0.1
 
-LFR_input = " -N " + str(N) + " -k "     + str(k) + " -maxk "  + str(maxk) + " -mu "    + str(mu) + " -t1 "    + str(t1) + " -t2 "    + str(t2) + " -minc "  + str(minc) + " -maxc "  + str(maxc) 
+def runCentAverage(args):
+    args['nmi_GN'] =0
+    args['nmi_CLAUSET'] =0
+    for i in range(100):
+        g = getLFR(args)
+        g_GN = g.copy()
+        g_CLAUSET = g.copy()
+        
+        top_level_communities = getCommunitiesLFR(g)
+        # if(args['N'] < 5000):
+            # top_level_communities_GN = getCommunitiesGN(g_GN)
+        top_level_communities_CLAUSET = getCommunitiesCLAUSET(g_CLAUSET)
+        
+        # print("Generated " + str(len(top_level_communities)) + " communities in the graph")
+        # print("GN Found " + str(len(top_level_communities_GN)) + " communities in the graph")
+        # print("CLAUSET Found " + str(len(top_level_communities_CLAUSET)) + " communities in the graph")
+        
+        draw(g, top_level_communities, 'spec')
+        # plt.figure(2)
+        # draw(g_GN, top_level_communities_GN, 'spec')
+        plt.figure(3)
+        draw(g_CLAUSET, top_level_communities_CLAUSET, 'spec')
 
-# create a modular graph
-# n =     300
-# k =     20
-# maxk =  160
-# mu =    0.2#0.1
-# tau1 =  3#2.0
-# tau2 =  1.5#1.1
-# minc =  90
-# maxc =  70
+        # if(args['N'] < 5000):
+        #     args['nmi_GN'] += nmi(get_nodes_community_list(g), get_nodes_community_list(g_GN))
 
-for i in range(1):
-    # mu += 0.1
-    print("Start LFR generation")
-    # g = LFR_benchmark_graph(n, tau1, tau2, mu, average_degree=k, max_degree=maxk, min_community=minc,  seed=10, max_iters=1000, tol=0.45) 
-    # try:
-    subprocess.run(["./benchmark" +LFR_input] , shell=True)
+        args['nmi_CLAUSET'] += nmi(get_nodes_community_list(g), get_nodes_community_list(g_CLAUSET))
     
-    # except Exception:
-    #     pass
-    network_path = './network.dat'
-    communities_path = './community.dat'
-    g = nx.read_edgelist(network_path)
-    
-    print("Finnished LFR generation")
-    # print(g)
-    top_level_communities = sorted(nxcom.greedy_modularity_communities(g), key=len, reverse=True)
-    # top_level_communities = nx.read_adjlist(network_path)
-    # top_level_communities = {frozenset(g.nodes[v]["community"]) for v in g}
-    
-    print("Found " + str(len(top_level_communities)) + " communities in the graph")
-
-    node_color_array=nodecolours(top_level_communities)
-    # print(node_color_array)
-
-
-    draw(g, node_color_array);
+    args['nmi_GN'] /= 100
+    args['nmi_CLAUSET'] /= 100
+    return args
 
 
 
-# print("Start Girvan Newman for communities")
-# communities_generator = community.girvan_newman(g)
-# top_level_communities_GN = next(communities_generator)
-# print("Finnished Girvan Newman")
-# print("Found " + str(len(top_level_communities_GN)) + " communities in the graph")
-# print("Assigning colours to communities")
-
-# print("Found " + str(len(top_level_communities_GN)) + " communities in the graph")
-
-# node_color_array_GN=nodecolours(top_level_communities_GN)
-# print(node_color_array_GN)
-
-# for i in range(0,len(node_color_array)):
-#     print(node_color_array[i] == node_color_array_GN[i])
 
 
-# draw(g, node_color_array_GN);
+# LFR graph generation parameters
+lfrArgs = {
+    'N':     200  , # variable 1000 or 5000
+    'k':     20   ,   # static
+    'maxk':  50   ,   # static
+    'mu':    0.2  ,    # For 0 to 1 in steps
+    't1':  2      ,   # static
+    't2':  1      ,   # static
+
+    'minc':  10   ,   # variable [10 and 50] or [20 and 100]
+    'maxc':  50,
+}
+
+
+# runSinglesMuIncrease(lfrArgs)
+resultStats = runCentAverage(lfrArgs)
+
+print("----------------------------------------------------------------------------")
+print("mu = " + str(lfrArgs['mu']))
+print("Normalized mutual information GN: " + str(resultStats['nmi_GN']))
+print("Normalized mutual information CLAUSET: " + str(resultStats['nmi_CLAUSET']))
+print("----------------------------------------------------------------------------")
