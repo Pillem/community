6d27c69db016e4c7ace1c7257292d58c9fb1bd3b
Pim de Vroom <p.j.vroom@students.uu.nl>
2021-06-05 19:58:14 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+echo "Building c++ program for lfr and ba graphs"
+make
+echo "Installing python3 dependencies"
+echo "(This assumes pip is installed)"
+pip install -r requirements.txt
+echo "Installation complete, run program with: python3 main.py"
+
